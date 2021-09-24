10b0232f3c9fa4968133feea66717befd5f4de5a
shehan C shanaka <39334423+shehancshanaka@users.noreply.github.com>
2021-09-24 17:38:58 +0530

    add the base layout for app

--- lms/src/App.js
+++ lms/src/App.js
@@ -1,11 +1,21 @@
 import styled from "styled-components";
+import { Footer, Header, Main } from "./components/Layout";
 
 const Title = styled.h1`
   color: red;
 `;
 
 function App() {
-  return <Title> hellow</Title>;
-}
+  return (
+  
+    <> 
+      <Header>This is the header</Header>
+      <Main>This is the main</Main>
+      <Footer> This is the footer</Footer>
 
+
+</>
+
+  )
+};
 export default App;
