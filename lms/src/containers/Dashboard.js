3e8aed4cc1c5044bb4bd197ca35eaf26d67f91e5
shehan C shanaka <39334423+shehancshanaka@users.noreply.github.com>
2021-10-03 00:14:57 +0530

    add axios and http reqest

--- lms/src/containers/Dashboard.js
+++ lms/src/containers/Dashboard.js
@@ -1,8 +1,30 @@
-import React, { useState } from "react";
-import HeaderImage from "../components/Spinner";
+import React, { useState,useEffect } from "react";
 import Tabs from "../components/Tabs";
+import { getBooks } from "../../api/bookAPI";
+
+
 
 const Dashboard = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [books, setBooks] = useState(null);
+
+useEffect(() => {
+  setIsLoading(true);
+  getBooks()
+    .then((response) => {
+      if (!response.error) setBooks(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    .finally(() => {
+      setIsLoading(false);
+    });
+}, []);
+
+
+
+
   const contents = [
   {title:"Books", elements:<h1>Contents of books go here</h1>},
   {title:"Members", elements:<h1>Contents of members go here</h1>},
