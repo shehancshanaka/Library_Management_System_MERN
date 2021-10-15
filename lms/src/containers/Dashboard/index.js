8f177242c8114eba36b60c3636d8cce88f5c16cb
shehan C shanaka <39334423+shehancshanaka@users.noreply.github.com>
2021-10-15 06:10:45 +0530

    use redux store in Dashboard

--- lms/src/containers/Dashboard/index.js
+++ lms/src/containers/Dashboard/index.js
@@ -1,38 +1,41 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setBooks } from "../../store/booksSlice";
 import Tabs from "../../components/Tabs";
 import { getBooks } from "../../api/bookAPI";
 import HeaderImage from "../../components/Spinner";
 import loading from "../../assets/Spin-1s-200px.gif";
 import Books from "./Books";
 
-
-
 const Dashboard = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const [books, setBooks] = useState([]);
+  //const [books, setBooks] = useState([]);
 
-useEffect(() => {
-  setIsLoading(true);
-  getBooks()
-    .then((response) => {
-      if (!response.error) setBooks(response.data);
-      console.log(response.data);
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-    .finally(() => {
-      setIsLoading(false);
-    });
-}, []);
+  const booksFromRedux = useSelector((state) => state.books.value);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    setIsLoading(true);
+    getBooks()
+      .then((response) => {
+        if (!response.error) {
+          console.log(setBooks(response.data));
+          dispatch(setBooks(response.data));
 
-
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [dispatch]);
 
   const contents = [
-  {title:"Books", elements:<Books catalog={books}></Books>},
-  {title:"Members", elements:<h1>Contents of members go here</h1>},
-]
+    { title: "Books", elements: <Books catalog={booksFromRedux}></Books> },
+    { title: "Members", elements: <h1>Contents of members go here</h1> },
+  ];
 
   return isLoading ? (
     <HeaderImage src={loading} />
